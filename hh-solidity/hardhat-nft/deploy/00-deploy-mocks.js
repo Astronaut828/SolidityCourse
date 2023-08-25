ee176a9ae9699d4b81be0bd14f998fe0f6d3ff3e
Astronaut828 <125616425+Astronaut828@users.noreply.github.com>
2023-08-25 22:29:39 +0000

    NFT lesson done (No Pinata)

--- hh-solidity/hardhat-nft/deploy/00-deploy-mocks.js
+++ hh-solidity/hardhat-nft/deploy/00-deploy-mocks.js
@@ -3,6 +3,8 @@ const { deploymentChains } = require("../helper-hardhat-config")
 
 const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+const DECIMALS = "18";
+const INITIAL_PRICE = "200000000000000000000";
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
@@ -18,6 +20,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
             log: true,
             args: args,
         })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
+        })
+
         log("Mocks Deployed!")
         log("------------------------------------")
         log("You are deploying to a local network, you'll need a local network running to interact")
