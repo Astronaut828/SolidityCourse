f1030d8115db70f843b8cef267c8f4413b4025d1
Astronaut828 <125616425+Astronaut828@users.noreply.github.com>
2023-08-30 18:53:00 +0000

    frontend graph

---
+++ hh-solidity/nextjs-nft-marketplace-thegraph/pages/_app.js
@@ -0,0 +1,22 @@
+import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+
+function App({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="../public/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
+}
+
+export default App
